0fed7a2f9a4524d665224e82ee1bf00550c5c017
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2021-12-19 17:29:49 +0100

    orga changes

--- jupyter_notebook/gpt_subtopic_creator.py
+++ jupyter_notebook/gpt_subtopic_creator.py
@@ -7,30 +7,28 @@ with open("openai_key.txt") as file:
     key = file.read()
     openai.api_key = key
 
-# GPT Model to create bulletpoints from a topic
-gpt_point_creation = GPT(engine="davinci", temperature=.5, max_tokens=120)
+# GPT Model to create subtopics from a topic
+gpt_subtopic_creation = GPT(engine="davinci", temperature=.5, max_tokens=120)
 
-gpt_point_creation.add_example(Example("car",
+gpt_subtopic_creation.add_example(Example("car",
                                        "motor, vehicle, transportation, road, wheels"
                                        ))
 
-gpt_point_creation.add_example(Example("tiger",
+gpt_subtopic_creation.add_example(Example("tiger",
                                        "cat, panthera, predator, wildlife, endangered"
                                        ))
 
-gpt_point_creation.add_example(Example("blockchain",
+gpt_subtopic_creation.add_example(Example("blockchain",
                                        "crypto, cryptography, transaction, bitcoin, cryptocurrency"
                                        ))
 
-gpt_point_creation.add_example(Example("germany",
+gpt_subtopic_creation.add_example(Example("germany",
                                        "country, europe, berlin, european union, demogracy"
                                        ))
 
 
 # Create Text (Bulletpoints) from a topic
 def create_subtopics_from_topic(prompt):
-    output = gpt_point_creation.submit_request(prompt)
+    output = gpt_subtopic_creation.submit_request(prompt)
     text_output = output.choices[0].text[8:]
     return text_output
-
-#print(create_subtopics_from_topic("computer"))
---
+++ jupyter_notebook/ipynb files/README.txt
@@ -0,0 +1,9 @@
+Running the script
+------------------
+- Create a txt file in this directory, name it "openai_key.txt" and copy your openai key into it
+- run the file "MAIN_gpt_markdown_creator.ipynb"
+
+Installing modules
+------------------
+- openai = pip install openai
+- gpt (is already in project as "gpt.py") = https://github.com/shreyashankar/gpt3-sandbox/blob/master/api/gpt.py
